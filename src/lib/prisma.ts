766e795e2159b5235072e02e6b40e3e3663eaeb3
Koutouan <kyfran6@gmail.com>
2026-06-04 16:26:38 +0000

    Final attempt at automatic deployment bypass

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,11 +4,6 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter: {
-      provider: 'sqlite',
-      url: 'file:./dev.db',
-    } as any
-  })
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
